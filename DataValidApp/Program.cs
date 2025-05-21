492c0c862ade15a5729bfebaf2ea9d83eed0a910
Graham Arnett <GrahamKentArnett@outlook.com>
2025-05-21 17:29:12 -0400

    skeleton is there

--- DataValidApp/Program.cs
+++ DataValidApp/Program.cs
@@ -6,9 +6,11 @@
         {
             Console.WriteLine("Data Validation App");
             char redo;
-
+            //there isn't a straight up data validation project in the book, so I'm making a version of the perimeter calc with it
             do
             {
+                Console.Write("\nEnter Length: ");
+                double length = double.Parse(Console.ReadLine());
                 redo =Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
             Console.WriteLine("Goodbye!");
