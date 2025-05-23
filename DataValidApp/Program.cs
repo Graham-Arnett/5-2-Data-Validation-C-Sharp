024ec7a7a3c743ede2c83862890959269479dfb1
Graham Arnett <GrahamKentArnett@outlook.com>
2025-05-23 17:22:57 -0400

    validation doesn't work yet

--- DataValidApp/Program.cs
+++ DataValidApp/Program.cs
@@ -10,16 +10,27 @@
             do
             {
                 Console.Write("\nEnter Length: ");
-                double length = double.Parse(Console.ReadLine());
+                double length = double.TryParse(Console.ReadLine);
                 Console.Write("\nEnter Width");
+                double width = double.Parse(Console.ReadLine());
+
+                double perimeter = (width * 2) + (length * 2);
+                double area = width * length;
+
+                Console.WriteLine($"\nThe perimeter of your rectangle is: {perimeter}" +
+                    $"\nThe area of your rectangle is: {area}");
 
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo =Console.ReadKey().KeyChar;
                 if (redo != 'y' && redo != 'Y' && redo != 'N' && redo != 'n')
                 {
-                    Console.WriteLine("That is not a valid input, try again.");
-                    break;
-                } 
+                    Console.WriteLine("\nThat is not a valid input, try again.");
+                    
+                }
+                else
+                {
+
+                }
             } while (redo == 'y' || redo == 'Y');
             Console.WriteLine("Goodbye!");
         }
