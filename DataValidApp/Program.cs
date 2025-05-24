8db901c9dac74848f64b4bbf593a78ba44f7838d
Graham Arnett <GrahamKentArnett@outlook.com>
2025-05-24 13:49:15 -0400

    we're closer but not quite

--- DataValidApp/Program.cs
+++ DataValidApp/Program.cs
@@ -7,12 +7,23 @@
             Console.WriteLine("Data Validation App");
             char redo;
             //there isn't a straight up data validation project in the book, so I'm making a version of the perimeter calc with it
+            double length = 0;
+            double width = 0;
             do
             {
                 Console.Write("\nEnter Length: ");
-                double length = double.TryParse(Console.ReadLine);
-                Console.Write("\nEnter Width");
-                double width = double.Parse(Console.ReadLine());
+                //double length = double.Parse(Console.ReadLine());
+                while(!double.TryParse(Console.ReadLine(), out length))
+                {
+                    Console.WriteLine("Invalid format, please enter a number!");
+                }
+                
+                Console.Write("\nEnter Width: ");
+                while (!double.TryParse(Console.ReadLine(), out width))
+                {
+                    Console.WriteLine("Invalid format, please enter a number!");
+                }
+
 
                 double perimeter = (width * 2) + (length * 2);
                 double area = width * length;
@@ -21,16 +32,20 @@
                     $"\nThe area of your rectangle is: {area}");
 
                 Console.Write("\n\nWould you like to go again? (y/n): ");
-                redo =Console.ReadKey().KeyChar;
-                if (redo != 'y' && redo != 'Y' && redo != 'N' && redo != 'n')
+                while (!char.TryParse(Console.ReadLine(), out redo) && redo != 'y' && redo != 'n')
                 {
-                    Console.WriteLine("\nThat is not a valid input, try again.");
+                    Console.WriteLine("That is not a valid input, try again please.");
+                }
+                //redo =Console.ReadKey().KeyChar;
+                //if (redo != 'y' && redo != 'Y' && redo != 'N' && redo != 'n')
+                //{
+                //    Console.WriteLine("\nThat is not a valid input, try again.");
                     
-                }
-                else
-                {
+                //}
+                //else
+                //{
 
-                }
+                //}
             } while (redo == 'y' || redo == 'Y');
             Console.WriteLine("Goodbye!");
         }
